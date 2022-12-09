a3531ab7339e4c6ceede46b96569860e543b239d
Puker Who <nawwesele@gmail.com>
2022-12-09 12:25:51 +0200

    edit

--- functions.php
+++ functions.php
@@ -256,3 +256,12 @@ function get_terms_links_array($hotel_id) {
     return $hotel_links;
   }
 }
+
+function wpb_imagelink_setup() {
+    $image_set = get_option( 'image_default_link_type' );
+     
+    if ($image_set !== 'none') {
+        update_option('image_default_link_type', 'none');
+    }
+}
+add_action('admin_init', 'wpb_imagelink_setup', 10);
