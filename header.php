90d77993917775e29553632ae045fe15ca522105
Puker Who <nawwesele@gmail.com>
2024-05-17 13:57:49 +0300

    Edit

--- header.php
+++ header.php
@@ -182,7 +182,7 @@
             </div>
             <div class="flex items-center">
               
-              <div class="hidden xl:flex"><a href="<?php echo get_page_url('page-add'); ?>" class="bg-red-400 hover:bg-red-500 text-white font-medium rounded-lg px-6 py-2"><?php _e("Добавить", "treba-wp"); ?> +</a></div>
+              <div class="hidden xl:flex"><a href="<?php echo get_page_url('page-contacts'); ?>" class="bg-red-400 hover:bg-red-500 text-white font-medium rounded-lg px-6 py-2"><?php _e("Контакты", "treba-wp"); ?></a></div>
               <div class="xl:hidden text-blue-500 cursor-pointer modal-js" data-modal="menu">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
                   <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd" />
